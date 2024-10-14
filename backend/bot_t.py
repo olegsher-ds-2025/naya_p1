0e37eb26bf0b13ffe1385ff9ff42e514c58ce6e6
Oleg Sher <oleg@sher.biz>
2024-10-14 15:26:26 +0300

    bot fixed and changes in flask

--- backend/bot_t.py
+++ backend/bot_t.py
@@ -1,51 +1,48 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, filters
 from api import work_with_series
+
 
 # Your bot token from BotFather
 TOKEN: str = '7265686775:AAHvlpXuwR53HX7u2TAqxxecAUV2CJxqgRI'
-z = work_with_series()
+DEV_BOT_TOKEN: str = '7529871846:AAG16iO9f1vn5-0Vq1CYfV30HMKkn5w44yM'
+rate = work_with_series()[2]
+date_of_course = work_with_series()[1]
+l_currencies = [key for key in rate.keys()]
 
 
 # Function to handle the /start command
 def start(update, context) -> None:
-    update.message.reply_text('Hello! I am your simple Telegram bot.')
+    update.message.reply_text('Hello! I am your simple Telegram Currency rate bot.')
+    update.message.reply_photo('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6mHwMCHABuF4qIQkUphrfhdJQTvYJKLGxhvCBdoypb63HQpRgTOTdZEzwZk6g7R2nEG8&usqp=CAU')
 
 # Function to handle text messages
 def handle_message(update, context) -> None:
     user_message = update.message.text
-    # if user_message == 'GBP':
-    #     update.message.reply_text(f'GBP/EUR {z.get(" GBP")[0]}')
-    # elif user_message == 'USD':
-    #     update.message.reply_text(f'USD/EUR {z.get(" USD")[0]}')
-    # elif user_message == 'ILS':
-    #     update.message.reply_text(f'ILS/EUR {z.get(" ILS")[0]}')
-    # else:
-    #     update.message.reply_text(f'You said: {user_message}')
-    update.message.reply_text(f'You said: {user_message}')
+    if user_message in l_currencies:
+        update.message.reply_text(f'1 EUR is equal {rate[user_message]} {user_message} for {date_of_course}')
+    else:
+        update.message.reply_text(f'You said: {user_message}')
+
 
 # Function to handle unknown commands
 def unknown(update, context) -> None:
     update.message.reply_text('Sorry, I didn’t understand that command.')
 
 def main() -> None:
-    print(z.get(' USD'))
+    # print(z.get(' USD'))
     # Create an Updater object and Dispatcher
-    updater = Updater(TOKEN, use_context=True)
+    updater = Updater(DEV_BOT_TOKEN, use_context=True)
     dispatcher = updater.dispatcher
-
-    # Add command handler for /start
     dispatcher.add_handler(CommandHandler('start', start))
-
-    # Add message handler for regular text messages
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
-
-
-    # Add handler for unknown commands
     dispatcher.add_handler(MessageHandler(Filters.command, unknown))
-
-    # Start the bot
     updater.start_polling()
     updater.idle()  # Keep the bot running until stopped
 
+
+
+
 if __name__ == '__main__':
     main()
+    
+    
--- frontend/__init__.py
+++ frontend/__init__.py
@@ -1,16 +1,22 @@
 from flask import Flask, redirect, url_for, request, render_template
 from backend.api import work_with_series
 
+
+rate = work_with_series()[2]
+date_of_course = work_with_series()[1]
+l_currencies = [key for key in rate.keys()]
+print(rate)
+
 app = Flask(__name__)
 
-data_single_day = work_with_series()
+
 # current_data_day = get_current_data_day()
 
 @app.route('/')
 def home():   
-    return render_template('index.html', data_single_day=data_single_day)
+    return render_template('index.html', data_single_day=rate, data_single_day_date=date_of_course)
 
 
 @app.route('/rates')
 def rates():
-    return render_template('my_file.html', data_single_day = data_single_day)
+    return render_template('my_file.html', data_single_day=rate)
