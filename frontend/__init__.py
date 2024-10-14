e5fb15d42d6699b890f4b07503c0fca644f65a7d
Oleg Sher <oleg@sher.biz>
2024-10-14 13:13:22 +0300

    refactoring __init__

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask, redirect, url_for, request, render_template
-from backend.api import work_with_series, get_current_data_day
+from backend.api import work_with_series
 
 app = Flask(__name__)
 
 data_single_day = work_with_series()
-current_data_day = get_current_data_day()
+# current_data_day = get_current_data_day()
 
 @app.route('/')
 def home():   
-    return render_template('index.html', data_single_day=data_single_day, current_data_day = current_data_day)
+    return render_template('index.html', data_single_day=data_single_day)
 
 
 @app.route('/rates')
